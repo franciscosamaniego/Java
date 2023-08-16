eebb3fde3b33d1d632c17909cd02347ca8ac780c
franciscosamaniego <102254119+franciscosamaniego@users.noreply.github.com>
2023-08-16 09:57:03 -0300

    Update Clase.java

--- Clase.java
+++ Clase.java
@@ -1,4 +1,10 @@
 import java.util.Scanner;
+//java: ejercicios basicos
+//1) Escribir por consola los numeros del 1 al 100 e informar si el mismo es par, imprimir su valor por pantalla.
+//2) Escribir por consola los numeros del 50 al 100 e informar si es un numero primo, sino escribir sus divisores.
+//3) Pedir numeros por pantalla solo hasta que alguno sea mayor a 100 y ademas sea numero primo.
+//4) Realizar una calculadora con las operaciones aritmeticas simples(suma, resta, multiplicacion y division).
+//5) Pensar y desarrollar un ejemplo donde se vea el uso de for each y comentarlo pertinentemente
 
 public class Clase {
 	public static void main(String[] args) {
