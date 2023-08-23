da5b0023751d6efb84623b0a08ab3549b2353c4b
franciscosamaniego <102254119+franciscosamaniego@users.noreply.github.com>
2023-08-23 09:54:56 -0300

    Update Ejercicio4.java

--- Chars/Ejercicio4.java
+++ Chars/Ejercicio4.java
@@ -13,6 +13,9 @@ public class Ejercicio4 {
 		System.out.println("Ingrese la letra que quiere que reemplaze la letra anterior");
 		c = scanner.next();
 		System.out.println("Palabra original: " + palabra);
+		//este for recorre toda la palabra que ingreso el usuario para luego comparar cada caracter con el caracter
+		//que desea reemplazar el usuario. Si hay un caracter que es igual, este se reemplaza por el nuevo caracter que ingreso el
+		//usuario
 		for(int i = 0; i < palabra.length(); i++) {
 			if(palabra.charAt(i) == b.charAt(0)) {
 				palabra = palabra.replace(palabra.charAt(i), c.charAt(0));
@@ -20,6 +23,7 @@ public class Ejercicio4 {
 			}
 			
 		}
+		//si no se encuentra ningun caracter que sea igual al que se ingreso, le avisara al usuario.
 		if(cont == 0) {
 			System.out.println("La letra que elegiste no esta en la palabra");
 			System.exit(0);
