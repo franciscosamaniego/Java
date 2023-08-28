4d4818e6b318492d5fd861e3fd16e39ccd003578
franciscosamaniego <102254119+franciscosamaniego@users.noreply.github.com>
2023-08-28 14:34:24 -0300

    Update Ejercicio6.java

--- Chars/Ejercicio6.java
+++ Chars/Ejercicio6.java
@@ -6,7 +6,7 @@ public class Ejercicio6 {
 		Scanner scanner = new Scanner(System.in);
 		String w1, w2, w3, w4, w5;
 		int l1, l2, l3, l4, l5;
-		
+		//Ingreso de palabras
 		System.out.println("Ingrese la primera palabra");
 		w1 = scanner.next();
 		System.out.println("Ingrese la segunda palabra");
@@ -17,17 +17,19 @@ public class Ejercicio6 {
 		w4 = scanner.next();
 		System.out.println("Ingrese la quinta palabra");
 		w5 = scanner.next();
-		
+		//Variables con la longitud de cada palabra
 		l1 = w1.length();
 		l2 = w2.length();
 		l3 = w3.length();
 		l4 = w4.length();
 		l5 = w5.length();
-		
+		//array con cada palabra y con las longitudes
 		String [] words = {w1,w2,w3,w4,w5};
 		int [] lengths = {l1,l2,l3,l4,l5};
 		int max = 0;
 		int idx = 0;
+		//for que compara cada longitud de cada palabra y si la longitud es mayor que la variable, se guarda la longitud y el indice
+		//en el que se ubica para luego mostrarlo en pantalla
 		for(int i = 0; i < 5; i++) {
 			if(max < lengths[i]) {
 				max = lengths[i];
