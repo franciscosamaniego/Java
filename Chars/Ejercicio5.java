99ba8eb7f4c096ae723e2af8f186cb939277e5af
franciscosamaniego <102254119+franciscosamaniego@users.noreply.github.com>
2023-08-28 14:32:42 -0300

    Update Ejercicio5.java

--- Chars/Ejercicio5.java
+++ Chars/Ejercicio5.java
@@ -9,6 +9,9 @@ public class Ejercicio5 {
 	w1 = scanner.next();
 	System.out.println("Ingrese la segunda palabra");
 	w2 = scanner.next();
+		//primero compara que las longitudes sean iguales, ya que para que una palbra sea anagrama de otra tienen que tener la misma
+		//cantidad de caracteres. Luego, con dos for se recorre para comparar que los caracteres que hay en una palabra esten en otras.
+		//Al sumarle a un contador, si este es igual a la longitud de las palbras, significa que son anagramas
 	if(w2.length() == w1.length()) {
 		int cont = 0;
 		for(int i = 0; i < w1.length(); i++) {
@@ -19,6 +22,7 @@ public class Ejercicio5 {
 				}
 			}
 		}
+		//Si no son anagramas, se le avisa al usuario
 		if(cont == w1.length()) System.out.println("Las palabra " + w1 + " es anagrama de la palabra " + w2);
 		else System.out.println("Las palabras no son anagramas");
 	}
