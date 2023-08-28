7271be3d29764a22b95142029f3c6d827c0d9110
franciscosamaniego <102254119+franciscosamaniego@users.noreply.github.com>
2023-08-28 14:23:51 -0300

    Update Ejercicios.java

--- Chars/Ejercicios.java
+++ Chars/Ejercicios.java
@@ -5,6 +5,7 @@ public class Ejercicios {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		String nombre;
+		//Programa que le pide al usuario una palabra y esta se imprime mas un saludo
 		System.out.println("Ingrese su nombre");
 		nombre = scanner.next();
 		System.out.println("Bienvenido, " + nombre);
